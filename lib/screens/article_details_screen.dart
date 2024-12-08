1e4a756a1d12cf4190a24a0b99dc713250a4aaf4
Sanmoy1 <emailofsanmoyroy@gmail.com>
2024-12-08 13:41:34 +0530

    article url link working

--- lib/screens/article_details_screen.dart
+++ lib/screens/article_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../models/article.dart';
 import '../providers/news_provider.dart';
 
@@ -63,9 +64,58 @@ class ArticleDetailsScreen extends StatelessWidget {
                   ),
                   const SizedBox(height: 16),
                   ElevatedButton.icon(
-                    onPressed: () {
-                      // TODO: Implement URL launching
-                      // We'll need to add url_launcher package for this
+                    onPressed: () async {
+                      if (article.url.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Article URL is not available'),
+                          ),
+                        );
+                        return;
+                      }
+
+                      try {
+                        final Uri url = Uri.parse(article.url);
+                        if (!url.hasScheme) {
+                          // Add https if scheme is missing
+                          final urlString = 'https://${article.url}';
+                          final httpsUrl = Uri.parse(urlString);
+                          if (await canLaunchUrl(httpsUrl)) {
+                            await launchUrl(
+                              httpsUrl,
+                              mode: LaunchMode.externalApplication,
+                              webViewConfiguration: const WebViewConfiguration(
+                                enableJavaScript: true,
+                                enableDomStorage: true,
+                              ),
+                            );
+                          } else {
+                            throw 'Could not launch $urlString';
+                          }
+                        } else {
+                          if (await canLaunchUrl(url)) {
+                            await launchUrl(
+                              url,
+                              mode: LaunchMode.externalApplication,
+                              webViewConfiguration: const WebViewConfiguration(
+                                enableJavaScript: true,
+                                enableDomStorage: true,
+                              ),
+                            );
+                          } else {
+                            throw 'Could not launch ${url.toString()}';
+                          }
+                        }
+                      } catch (e) {
+                        if (context.mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Could not open the article: ${e.toString()}'),
+                              duration: const Duration(seconds: 3),
+                            ),
+                          );
+                        }
+                      }
                     },
                     icon: const Icon(Icons.launch),
                     label: const Text('Read Full Article'),
